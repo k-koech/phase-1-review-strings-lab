7d7cc408e41bc174b13ae64650bd2d157af4e9c9
k-koech <kelvin.koech@student.moringaschool.com>
2022-05-31 09:06:38 +0300

    commit tree dc3d6f133efb34bdb931087c20ded41de57bb2a6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+var currentUser = 'Grace Hopper';
+
+var welcomeMessage = 'Welcome to Flatbook, ' + currentUser+'!';
+var excitedWelcomeMessage="WELCOME TO FLATBOOK, !";
+
+currentUser=excitedWelcomeMessage;
+
+var welcomeMessage = 'Welcome to Flatbook, ' + currentUser+'!';
+
+var shortGreeting = "Welcome, !";
+
